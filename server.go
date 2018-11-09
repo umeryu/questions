25571ce53f5c0acc261f65bc0438d13aa5687667
梅川 竜一 <u.ryuichi@gmail.com>
2018-11-09 23:49:06 +0900

    1 cool finish

--- server.go
+++ server.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Page struct { // テンプレート展開用データ構造
@@ -15,9 +17,18 @@ type Page struct { // テンプレート展開用データ構造
 type QuestionPage struct {
 	PageId      string
 	PageName    string
+	PageDisc    string
 	PageBackImg string
 	Questions   []*QuestionInfo
-	Status      map[string]string
+	Status      StatusInfo
+}
+
+type StatusInfo struct {
+	ALL       int
+	OK        int
+	YET       int
+	PER       string
+	StatusStr []string
 }
 
 type QuestionInfo struct { // テンプレート展開用データ構造
@@ -56,9 +67,23 @@ func questionView(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("form parse erro")
 		}
 
-		cont := r.Form["status-info"]
-		fmt.Println("=> ", cont)
-		//  id, status
+		//  id-status:OK id-status:NG の文字列を取得できる
+		thispage.Status.StatusStr = r.Form["status-info"]
+		//fmt.Println("=> ", thispage.Status.StatusStr, "len:", len(thispage.Status.StatusStr))
+		all := len(thispage.Status.StatusStr)
+		okcount := 0
+		r := regexp.MustCompile("-status:OK")
+
+		for i := 0; i < all; i++ {
+			if r.MatchString(thispage.Status.StatusStr[i]) {
+				okcount++
+			}
+		}
+		thispage.Status.ALL = all
+		thispage.Status.OK = okcount
+		thispage.Status.YET = all - okcount
+		thispage.Status.PER = strconv.FormatFloat((float64(okcount)/float64(all))*100, 'f', 2, 64)
+
 		// 現状保存し、Statusに保存
 		// 現状の表示情報を取り出す。
 		// 保存状態からデータを作り直しmessegeエンハンス
@@ -78,9 +103,9 @@ func questionView(w http.ResponseWriter, r *http.Request) {
 //-----------------------------------------------------------------------------
 func loadPageInfo() {
 	thispage.PageId = "page1"
-	thispage.PageName = "first page"
-	thispage.PageBackImg = "./img/page1.png"
-
+	thispage.PageName = "背中：僧帽筋"
+	thispage.PageBackImg = "/img/page1.png"
+	thispage.PageDisc = "僧帽筋の起点、停止点を述べよ。"
 	createQuestionInfo()
 	thispage.Questions = messeges
 
@@ -88,44 +113,63 @@ func loadPageInfo() {
 
 func createQuestionInfo() int {
 	var count int = 0
-	count = 4
+	count = 6
 	messeges = make([]*QuestionInfo, count)
 	messeges[0] = &QuestionInfo{
 		"myid1",
-		"Question1",
-		"Answer1",
-		"100px",
-		"200px",
+		"起点1 2つ",
+		"仙骨、腸骨稜",
+		"300px",
+		"450px",
 		"L",
-		"OK",
+		"NG",
 	}
 	messeges[1] = &QuestionInfo{
 		"myid2",
-		"Question2",
-		"世界",
+		"起点2　1",
+		"腰椎L1-L5",
 		"300px",
-		"300px",
+		"400px",
 		"R",
-		"OK",
+		"NG",
 	}
 	messeges[2] = &QuestionInfo{
 		"myid3",
-		"Question3",
-		"世界",
-		"500px",
-		"500px",
+		"起点3 １つ",
+		"肋骨 9-12",
+		"310px",
+		"300px",
 		"R",
 		"NG",
 	}
 	messeges[3] = &QuestionInfo{
 		"myid4",
-		"Question4",
-		"世界",
-		"100px",
-		"500px",
+		"起点4 1つ",
+		"肩甲骨下角",
+		"310px",
+		"250px",
 		"L",
 		"NG",
 	}
+	messeges[4] = &QuestionInfo{
+		"myid5",
+		"起点5 1つ",
+		"大後頭骨隆起",
+		"250px",
+		"80px",
+		"L",
+		"NG",
+	}
+	messeges[5] = &QuestionInfo{
+		"myid6",
+		"終点 3つ",
+		"肩峰、肩甲骨突起、鎖骨外側1/3",
+		"330px",
+		"200px",
+		"L",
+		"NG",
+	}
+
 	return count
 }
 
@@ -135,5 +179,7 @@ func createQuestionInfo() int {
 func main() {
 	loadPageInfo()
 	http.HandleFunc("/q", questionView)
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+
 	http.ListenAndServe(":8080", nil)
 }
